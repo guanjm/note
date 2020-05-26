1ac200fe63c1bb485099849de468b4e24b7e1592
ex_guanjm <511190813@qq.com>
2020-05-26 18:58:59 +0800

    update

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -1,61 +1,38 @@
 #!/bin/bash
-mysql_path=/home/mysql/mysql-8.0.20-linux-x86_64-minimal
-basename=$(basename $PWD)
-port=${basename#mysql_}
-basedir=$PWD
-datadir=$basedir'/data'
-general_log_file=$basedir'/log/mysql.log'
-log_error=$basedir'/log/mysql-error.log'
-pid_file=$basedir'/tmp/mysql.pid'
-slow_query_log_file=$basedir'/log/mysql-slow.log'
-socket=$basedir'/tmp/mysql.sock'
-tmpdir=$basedir'/tmp'
-lc_messages_dir=$mysql_path'/share'
 
 init() {
-  tmpdir=$basedir'/tmp'
-  logdir=$basedit'/log'
+  datadir='./data'
+  tmpdir='./tmp'
+  logdir='./log'
+  logfile=$logdir'/mysql-error.log'
   if [ ! -e $tmpdir ]; then
     mkdir $tmpdir
   fi
   if [ ! -e $logdir ]; then
     mkdir $logdir
   fi
-  if [ ! -e $log_error ]; then
-    touch $log_error
+  if [ ! -e $logfile ]; then
+    touch $logfile
   fi
-  $mysql_path'/bin/mysqld' --initialize-insecure \
-                           --port=$port \
-                           --basedir=$basedir \
-                           --datadir=$datadir \
-                           --general-log-file=$general_log_file \
-                           --pid-file=$pid_file \
-                           --slow_query_log_file=$slow_query_log_file \
-                           --log-error=$log_error \
-                           --socket=$socket \
-                           --tmpdir=$tmpdir \
-                           --lc-messages-dir=$lc_messages_dir
+  '/home/mysql/mysql-8.0.20-linux-x86_64-minimal/bin/mysqld' --defaults-file=/home/mysql/mysql_3301/my.cnf \
+                                                             --initialize-insecure
+  if [ $? -eq 0 ]; then
+    echo 'init success!'
+  else
+    echo -e "init fail:"
+    [ -e $logfile ] && cat $logfile
+    rm -rf $datadir
+    rm -rf $tmpdir
+    rm -rf $logdir
+  fi
 }
 
 start() {
-  $mysql_path'/bin/mysqld' --port=$port \
-                           --basedir=$basedir \
-                           --datadir=$datadir \
-                           --general-log-file=$general_log_file \
-                           --pid-file=$pid_file \
-                           --slow_query_log_file=$slow_query_log_file \
-                           --log-error=$log_error \
-                           --socket=$socket \
-                           --tmpdir=$tmpdir \
-                           --lc-messages-dir=$lc_messages_dir
-}
-
-stop() {
-  echo 'stop'
+  nohup '/home/mysql/mysql-8.0.20-linux-x86_64-minimal/bin/mysqld' --defaults-file=/home/mysql/mysql_3301/my.cnf &
 }
 
 funType=$1
-while [[ -z $funType || ($funType != 'init' && $funType != 'start' && $funType != 'stop') ]]; do
+while [[ ($funType != 'init' && $funType != 'start' && $funType != 'stop') ]]; do
 	read -p 'please input funType [ init | start | stop ]: ' input
 	funType=$input
 done
